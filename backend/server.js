9c33317426b3cf020f8bf3c3b8cf6b644f5e7368
Navjyothari <navjyotpillai15@gmail.com>
2026-02-23 15:57:34 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+
+
+
 const express = require("express");
 const cors = require("cors");
 
@@ -13,6 +16,21 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     console.log("server is running");
 })
+
+
+const pool = require("./config/db");
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ connected: true, time: result.rows[0] });
+  } catch (err) {
+    console.error("DB ERROR 👉", err);   // ⭐ important
+    res.json({ connected: false, error: err.message });
+  }
+});
+console.log("DB URL:", process.env.DATABASE_URL);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
 
